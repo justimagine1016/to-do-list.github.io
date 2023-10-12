c554105a1636c01e7bb2b5e6e07e6b5edf421d1e
justimagine1016 <himanshusingh1016@gmail.com>
2023-10-12 17:10:31 +0530

    done

--- script.js
+++ script.js
@@ -21,9 +21,11 @@ listContainer.addEventListener(
   function (e) {
     if (e.target.tagName === "LI") {
       e.target.classList.toggle("checked");
-      
+      saveData();
+
     } else if (e.target.tagName === "SPAN") {
       e.target.parentElement.remove();
+      saveData();
     }
     saveData();
   },
